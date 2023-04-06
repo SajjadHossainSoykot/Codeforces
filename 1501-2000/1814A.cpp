f47069f40d52ba4169b09adff728ca2204ca9eb5
SajjadHossainSoykot <sajjadhossainsoykot@gmail.com>
2023-04-06 23:40:49 +0600

    1814A solved

---
+++ 1501-2000/1814A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        unsigned long long int n, k, y, diff;
+        cin>>n>>k;
+        if(n%2==0)cout<<"YES\n";
+        else if(k%2==1)cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+
+}
