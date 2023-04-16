0a30e2c3352d0a2a8c59e7e1c8573a9307cfe6b0
SajjadHossainSoykot <sajjadhossainsoykot@gmail.com>
2023-04-16 17:48:33 +0600

    1154A c++ solve

---
+++ 1001-1500/1154A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long x[4];
+    for (int i = 0; i < 4; i++)
+    {
+        cin>>x[i];
+    }
+    sort(x,x+4);
+    for (int i = 0; i < 3; i++)
+    {
+        cout<<x[3]-x[i]<<" ";
+    }
+    
+}    
