08460acf46648b8f1f30f8a042577631418452df
SajjadHossainSoykot <sajjadhossainsoykot@gmail.com>
2023-04-22 13:49:58 +0600

    1772A solved

---
+++ 1501-2000/1772A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        string s;
+        cin>>s;
+        int ans = s[0] + s[2] - 96;
+        cout<<ans<<endl;
+    }
+    
+}
