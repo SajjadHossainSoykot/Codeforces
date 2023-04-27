9e3b317fcd1c74527208d362169c8a9adfec5e05
SajjadHossainSoykot <sajjadhossainsoykot@gmail.com>
2023-04-28 00:31:34 +0600

    1660A solved

---
+++ 1501-2000/1660A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        long a,b;
+        cin>>a>>b;
+        if(a==0)cout<<1<<endl;
+        else cout<<a+b*2+1<<endl;
+    }
+}
