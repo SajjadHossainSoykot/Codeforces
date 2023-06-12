971ceb59bb6f331fc2ec5a4679396b3008f43e1c
SajjadHossainSoykot <sajjadhossainsoykot@gmail.com>
2023-06-13 01:29:38 +0600

    1549A Solved

---
+++ 1501-2000/1549A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long p;
+        cin>>p;
+        cout<<2<<" "<<p-1<<endl;
+    }
+}
