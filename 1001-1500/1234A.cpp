45e03e0d33a501c0a00618f59dba19e37fe3af33
SajjadHossainSoykot <sajjadhossainsoykot@gmail.com>
2023-07-09 12:23:16 +0600

    1234A solved

---
+++ 1001-1500/1234A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            sum += x;
+        }
+        cout << (sum + n - 1) / n << endl;
+    }
+}
