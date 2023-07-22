02210cc19582550ed20be568a7e283a6613be1ad
SajjadHossainSoykot <sajjadhossainsoykot@gmail.com>
2023-07-23 01:28:21 +0600

    1550A solved

---
+++ 1501-2000/1550A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int s;
+        cin>>s;
+        int ans = ceil(sqrt(s));
+        cout<<ans<<endl;
+    }
+}
