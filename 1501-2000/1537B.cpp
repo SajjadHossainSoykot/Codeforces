75ce28184bc946c883317db3551f03d42a6fb3a6
SajjadHossainSoykot <sajjadhossainsoykot@gmail.com>
2023-08-15 02:30:13 +0600

    1537B Solved

---
+++ 1501-2000/1537B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m, i, j;
+        cin>>n>>m>>i>>j;
+        printf("1 1 %d %d\n", n, m);
+    }
+}
+
